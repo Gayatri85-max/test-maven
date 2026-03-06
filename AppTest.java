8ff2684372a8e0e1cc404b750d4ba04dc99f0ecf
gayatri <gayathrikp82@gmail.com>
2026-03-06 16:35:51 +0530

    Create AppTest.java

---
+++ AppTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+
+        App app = new App();
+
+        int result = app.add(2, 3);
+
+        assertEquals(5, result);
+
+    }
+}
